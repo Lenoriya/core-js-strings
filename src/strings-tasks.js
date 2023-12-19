2c415c4ef4dc956ae48bdd09155e58cafb436122
Helen Kruminia <lena_kru@tut.by>
2023-12-19 13:31:17 +0300

    feat: Remove the first occurrence of a substring from a string.

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -170,8 +170,13 @@ function repeatString(/* str, times */ str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(/* str, value */ str, value) {
+  // throw new Error('Not implemented');
+  const index = str.indexOf(value);
+  if (index === -1) {
+    return str;
+  }
+  return str.slice(0, index) + str.slice(index + value.length);
 }
 
 /**
