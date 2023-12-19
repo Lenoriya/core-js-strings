f2c65f89bf2167e5358d8f36bb34a77db8a004eb
Helen Kruminia <lena_kru@tut.by>
2023-12-19 14:17:30 +0300

    feat: Returns a string in reverse order.

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -281,8 +281,9 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(/* str */ str) {
+  // throw new Error('Not implemented');
+  return str.split('').reverse().join('');
 }
 
 /**
